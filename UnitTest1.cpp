99bdb931f8ea307a0dab4505eb862f35c769c378
pawelkrempa22 <147718936+pawelkrempa22@users.noreply.github.com>
2023-12-21 00:59:52 +0100

    Add files via upload
    comit pierwsyz miohj

--- UnitTest1.cpp
+++ UnitTest1.cpp
@@ -1,7 +1,8 @@
-#include "pch.h"
+﻿#include "pch.h"
 #include "CppUnitTest.h"
-#include"../kalkulator/funkcje.cpp"
+#include "C:\Users\48729\Desktop\Project1\Project1\funkcje.cpp"
 #include <math.h>
+#include <cmath>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -10,7 +11,7 @@ namespace UnitTest1
 	TEST_CLASS(UnitTest1)
 	{
 	public:
-		
+
 		TEST_METHOD(TestMethod1)
 		{
 			Assert::AreEqual(log_nat(5.0), log(5.0));
@@ -19,9 +20,21 @@ namespace UnitTest1
 		{
 			Assert::AreEqual(exponent(5.0), exp(5.0));
 		}
-		/*TEST_METHOD(TestMethod3)
+		TEST_METHOD(TestOdchylenieStd)
 		{
-			Assert::AreEqual(odchylenie_std(tab), );
-		}*/
+			float arr[] = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+			float result = odchylenie_std(arr);
+			float expected = 1.41421; // Wartość oczekiwana 
+
+			Assert::AreEqual(expected, result);
+		}
+		TEST_METHOD(TestLogarytmZ10)
+		{
+			double input = 5.0;
+			double result = logarytm_z_10(input);
+			double expected = log10(input);
+
+			Assert::AreEqual(expected, result);
+		}
 	};
 }
---
+++ main (2).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "funkcje.h"
+#include <math.h>
+
+int main() {
+	float tab[5] = {1.0, 2.0, 3.0, 4.0, 5.0};
+	std::cout << "logarytm naturalny liczby 5.0 wynosi: " << log_nat(5.0) << std::endl;
+	std::cout << "odchylenie standardowe tablicy 5 elementow (tab) wynosi: " << odchylenie_std(tab)<<std::endl;
+	std::cout << "exponent liczby 5.0 wynosi " << exponent(5.0)<<std::endl;
+	std::cout << "logarytm liczby 5.0 z 10 wynosi " << logarytm_z_10(5.0) << std::endl;
+	return 0;
+}
